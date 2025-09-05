2b2a5d08e7d4d562d53ee28fe087a1dab26b5d5d
daw-ai <dawson@techascension.com>
2025-09-05 20:52:44 +0200

    Update main.jsx

--- techascension-website/src/main.jsx
+++ techascension-website/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Privacy from './Privacy.jsx';
+import TermsOfService from './TermsOfService.jsx';
+import { Router, Route } from './Router.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Root = () => {
+  return (
+    <Router>
+      <Route path="/" component={App} />
+      <Route path="/privacy" component={Privacy} />
+      <Route path="/terms" component={TermsOfService} />
+    </Router>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
